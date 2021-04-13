80e73ffeb43edd31228ed8e939650c48fdc7fb05
voidz8 <renzovermue@hotmail.com>
2021-04-13 12:13:24 +0200

    finished functions.js

--- functions.js
+++ functions.js
@@ -14,20 +14,32 @@ console.log(greeting);
 // Schrijf een functie genaamd getSchool, die de string "Novi Hogeschool" teruggeeft
 // ---- Verwachte uitkomst bij het aanroepen van de functie: "Novi Hogeschool"
 
+const getSchool = () => "Novi Hogeschool";
+
+console.log(getSchool());
+
 
 
 /* Opdracht 2 */
 // Schrijf een functie genaamd compliment, die jou een complimentje geeft!
 // ---- Verwachte uitkomst bij het aanroepen van de functie: "Lekker bezig met die functies, Jan!"
 
+const compliment = () => "Lekker bezig met die functies, Jan!";
 
+console.log(compliment());
 
 /* Opdracht 3 */
 // Schrijf een functie genaamd getGrades, die een array met de cijfers 8, 8.5, 6 en 7 teruggeeft.
 // ---- Verwachte uitkomst bij het aanroepen van de functie: [8, 8.5, 6, 7]
 
+const  getGrades = () => [8, 8.5, 6, 7];
 
+console.log(getGrades());
 
 /* Opdracht 4 */
 // Schrijf een functie genaamd getDetails, die een object met daarin jouw eigen naam en achternaam teruggeeft.
 // ---- Verwachte uitkomst bij het aanroepen van de functie: { firstName: 'Jan', lastName: 'Janssen' }
+
+const getDetails = (firstName, lastName) => firstName + " " + lastName;
+
+console.log(getDetails("Renzo", "Vermue"))
